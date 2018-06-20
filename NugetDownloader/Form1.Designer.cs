3161ff0c96334d4f774d1f5b781f4dbc67338351
eugenexlin <eugenexlin@gmail.com>
2018-06-20 02:01:56 -0700

    initial commit
    main UI and rudimentary validations done.

---
+++ NugetDownloader/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace NugetDownloader
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.btnDownload = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.btnDefault = new System.Windows.Forms.Button();
+			this.txtRemoteNugetPath = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.txtOutputReportPath = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtStagingNugetPath = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtLocalNugetPath = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.IsSplitterFixed = true;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.label8);
+			this.splitContainer1.Panel1.Controls.Add(this.label7);
+			this.splitContainer1.Panel1.Controls.Add(this.btnDownload);
+			this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+			this.splitContainer1.Panel1.Controls.Add(this.label5);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.btnDefault);
+			this.splitContainer1.Panel2.Controls.Add(this.txtRemoteNugetPath);
+			this.splitContainer1.Panel2.Controls.Add(this.label6);
+			this.splitContainer1.Panel2.Controls.Add(this.btnSave);
+			this.splitContainer1.Panel2.Controls.Add(this.txtOutputReportPath);
+			this.splitContainer1.Panel2.Controls.Add(this.label4);
+			this.splitContainer1.Panel2.Controls.Add(this.txtStagingNugetPath);
+			this.splitContainer1.Panel2.Controls.Add(this.label3);
+			this.splitContainer1.Panel2.Controls.Add(this.txtLocalNugetPath);
+			this.splitContainer1.Panel2.Controls.Add(this.label2);
+			this.splitContainer1.Panel2.Controls.Add(this.label1);
+			this.splitContainer1.Size = new System.Drawing.Size(584, 241);
+			this.splitContainer1.SplitterDistance = 360;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(12, 78);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(310, 38);
+			this.label8.TabIndex = 13;
+			this.label8.Text = "Info report html about all new nugets will be generated at the Report path.";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(12, 34);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(310, 38);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "Downloads nuget and entire dependency tree to Staging path. Will not download nug" +
+    "ets found in Local path.";
+			// 
+			// btnDownload
+			// 
+			this.btnDownload.Location = new System.Drawing.Point(15, 173);
+			this.btnDownload.Name = "btnDownload";
+			this.btnDownload.Size = new System.Drawing.Size(75, 23);
+			this.btnDownload.TabIndex = 11;
+			this.btnDownload.Text = "Download";
+			this.btnDownload.UseVisualStyleBackColor = true;
+			this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(15, 147);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(320, 20);
+			this.textBox1.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 131);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(268, 13);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Desired Nuget Package (url or package.version.nupkg)";
+			// 
+			// btnDefault
+			// 
+			this.btnDefault.Location = new System.Drawing.Point(88, 193);
+			this.btnDefault.Name = "btnDefault";
+			this.btnDefault.Size = new System.Drawing.Size(75, 23);
+			this.btnDefault.TabIndex = 10;
+			this.btnDefault.Text = "Default";
+			this.btnDefault.UseVisualStyleBackColor = true;
+			this.btnDefault.Click += new System.EventHandler(this.btnDefault_Click);
+			// 
+			// txtRemoteNugetPath
+			// 
+			this.txtRemoteNugetPath.Location = new System.Drawing.Point(7, 50);
+			this.txtRemoteNugetPath.Name = "txtRemoteNugetPath";
+			this.txtRemoteNugetPath.Size = new System.Drawing.Size(200, 20);
+			this.txtRemoteNugetPath.TabIndex = 9;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(4, 34);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(138, 13);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "Remote Nuget Path or URL";
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(7, 193);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 7;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// txtOutputReportPath
+			// 
+			this.txtOutputReportPath.Location = new System.Drawing.Point(7, 167);
+			this.txtOutputReportPath.Name = "txtOutputReportPath";
+			this.txtOutputReportPath.Size = new System.Drawing.Size(200, 20);
+			this.txtOutputReportPath.TabIndex = 6;
+			this.txtOutputReportPath.Leave += new System.EventHandler(this.txtPath_Leave);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(4, 151);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(99, 13);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Output Report Path";
+			// 
+			// txtStagingNugetPath
+			// 
+			this.txtStagingNugetPath.Location = new System.Drawing.Point(7, 128);
+			this.txtStagingNugetPath.Name = "txtStagingNugetPath";
+			this.txtStagingNugetPath.Size = new System.Drawing.Size(200, 20);
+			this.txtStagingNugetPath.TabIndex = 4;
+			this.txtStagingNugetPath.Leave += new System.EventHandler(this.txtPath_Leave);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(4, 112);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Staging Nuget Path";
+			// 
+			// txtLocalNugetPath
+			// 
+			this.txtLocalNugetPath.Location = new System.Drawing.Point(7, 89);
+			this.txtLocalNugetPath.Name = "txtLocalNugetPath";
+			this.txtLocalNugetPath.Size = new System.Drawing.Size(200, 20);
+			this.txtLocalNugetPath.TabIndex = 2;
+			this.txtLocalNugetPath.Leave += new System.EventHandler(this.txtPath_Leave);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(4, 73);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(90, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Local Nuget Path";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Cursor = System.Windows.Forms.Cursors.Cross;
+			this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(4, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Settings";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(584, 241);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Form1";
+			this.Text = "Nuget Downloader";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtLocalNugetPath;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtStagingNugetPath;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtOutputReportPath;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtRemoteNugetPath;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button btnDefault;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button btnDownload;
+	}
+}
+
---
+++ NugetDownloader/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NugetDownloader
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			loadSettings();
+		}
+
+		private void loadSettings()
+		{
+			txtRemoteNugetPath.Text = Properties.Settings.Default.RemoteNugetPath;
+			txtLocalNugetPath.Text = Properties.Settings.Default.LocalNugetPath;
+			txtStagingNugetPath.Text = Properties.Settings.Default.StagingNugetPath;
+			txtOutputReportPath.Text = Properties.Settings.Default.OutputReportPath;
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			// validations
+			string errors;
+			
+			errors = validatePath(txtLocalNugetPath.Text);
+			if (errors.Length > 0)
+			{
+				MessageBox.Show(errors);
+				return;
+			}
+			errors = validatePath(txtStagingNugetPath.Text);
+			if (errors.Length > 0)
+			{
+				MessageBox.Show(errors);
+				return;
+			}
+			errors = validatePath(txtOutputReportPath.Text);
+			if (errors.Length > 0)
+			{
+				MessageBox.Show(errors);
+				return;
+			}
+
+			Properties.Settings.Default.RemoteNugetPath = txtRemoteNugetPath.Text;
+			Properties.Settings.Default.LocalNugetPath = txtLocalNugetPath.Text;
+			Properties.Settings.Default.StagingNugetPath = txtStagingNugetPath.Text;
+			Properties.Settings.Default.OutputReportPath = txtOutputReportPath.Text;
+			Properties.Settings.Default.Save();
+			loadSettings();
+		}
+
+		private void txtPath_Leave(object sender, EventArgs e)
+		{
+			textLeaveHandler(((TextBox)sender).Text);
+		}
+
+		private void textLeaveHandler(string text)
+		{
+			string errors = validatePath(text);
+			if (errors.Length > 0)
+			{
+				MessageBox.Show(errors);
+				return;
+			}
+			else
+			{
+				if (!Directory.Exists(text))
+				{
+					promptCreatePath(text);
+					return;
+				}
+			}
+		}
+
+		private void promptCreatePath(string path)
+		{
+			var confirmResult = MessageBox.Show(
+									String.Format(
+										"Would you like to create the path '{0}' ?",
+										path
+									),
+									"Auto-generate Path",
+									MessageBoxButtons.YesNo
+								);
+			if (confirmResult == DialogResult.Yes)
+			{
+				Directory.CreateDirectory(path);
+			}
+			else
+			{
+				
+			}
+		}
+
+
+		private string validatePath(string path)
+		{
+			StringBuilder errors = new StringBuilder();
+			try
+			{
+				Path.GetFullPath(path);
+			}
+			catch
+			{
+				errors.AppendLine(
+					String.Format(
+						"Path '{0}' is invalid.",
+						path
+					)
+				);
+			}
+			if (!Path.IsPathRooted(path))
+			{
+				errors.AppendLine(
+					String.Format(
+						"Path '{0}' needs to be an absolute path.",
+						path
+					)
+				);
+			}
+			return errors.ToString();
+		}
+
+		private void btnDefault_Click(object sender, EventArgs e)
+		{
+			Properties.Settings.Default.Reset();
+			Properties.Settings.Default.Save();
+			loadSettings();
+		}
+
+		private void btnDownload_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
